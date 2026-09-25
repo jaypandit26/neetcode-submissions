439721c0bd2c7551053d597fbaf4a6ac882c0455
jaypandit26 <99516464+jaypandit26@users.noreply.github.com>
2026-09-25 10:19:44 +0530

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        //applying binary search
+
+        Arrays.sort(stones);
+        int n = stones.length;
+
+        while(n>1){
+            int current = stones[n-1] - stones[n-2];
+            n -= 2;
+            if(current > 0){
+                int l = 0, r = n;
+                while(l < r){
+                    int mid = (l + r) /2;
+                    if(stones[mid] < current){
+                        l = mid + 1;
+                    }else{
+                        r = mid;
+                    }
+                }
+                int pos = l;
+                n++;
+                stones = Arrays.copyOf(stones, n);
+                for(int i = n -1;i>pos;i--){
+                    stones[i] = stones[i-1];
+                }
+                stones[pos] = current;
+            }
+        }
+        return n > 0 ? stones[0] : 0;
+    }
+}
